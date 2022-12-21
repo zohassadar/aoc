606905a93890ba56a9c7ebd7d919a1d7fbea26bb
Richard <zohassadar@gmail.com>
2022-12-21 15:21:52 -0700

    working but not optimized star 1

--- 2022_d10-19/d19.py
+++ 2022_d10-19/d19.py
@@ -1,5 +1,5 @@
 import re
-
+from time import perf_counter
 import numpy as np
 from functools import cache
 
@@ -33,15 +33,23 @@ class Blueprint:
         self.index = index
         self.debug = debug
         self.ore_cost_ore = ore_cost_ore
+
         self.clay_cost_ore = clay_cost_ore
+
         self.obs_cost_ore = obs_cost_ore
         self.obs_cost_clay = obs_cost_clay
+
         self.geo_cost_ore = geo_cost_ore
         self.geo_cost_obs = geo_cost_obs
 
-        self.ore_max = self.clay_cost_ore + self.ore_cost_ore + self.geo_cost_ore
-        self.clay_max = self.obs_cost_clay
-        self.obs_max = self.geo_cost_obs
+        self.ore_max = sum(
+            (
+                self.clay_cost_ore,
+                self.ore_cost_ore,
+                self.geo_cost_ore,
+                self.obs_cost_ore,
+            )
+        )
 
         self.limit = 24
         self.args = (
@@ -57,91 +65,115 @@ class Blueprint:
         self.best_geo = 0
 
     @cache
+    def best_case(self, minute):
+        return sum(range(1, (self.limit - minute) + 3))
+
+    @cache
     def recurse(
         self,
-        minute,
-        ore,
-        oreR,
-        clay,
-        clayR,
-        obs,
-        obsR,
-        geos,
-        geosR,
+        minute=1,
+        ore=0,
+        oreR=1,
+        clay=0,
+        clayR=0,
+        obs=0,
+        obsR=0,
+        geos=0,
+        geosR=0,
     ):
+
         pad = minute * " "
         self.debug and print(
             pad
             + f"{minute=} {ore=} {oreR=} {clay=} {clayR=} {obs=} {obsR=} {geos=} {geosR=}"
         )
-        if minute >= self.limit:
+        if minute > self.limit:
+            self.debug and print(pad + f"Giving up at minute {minute} with {geos}")
             self.best_geo = max((geos, self.best_geo))
+            if geos == self.best_geo:
+                self.debug and print(
+                    pad
+                    + f"{minute=} {ore=} {oreR=} {clay=} {clayR=} {obs=} {obsR=} {geos=} {geosR=}"
+                )
+            return
+        remaining = self.limit - minute + 2
+        best_case = geos + (remaining * (remaining + 1) // 2)
+        if best_case <= self.best_geo:
+            self.debug and print(
+                pad
+                + f"Giving up at minute {minute} because {best_case} <= {self.best_geo}"
+            )
             return
 
-        if ore >= self.geo_cost_ore and obs >= self.geo_cost_obs:
-            self.recurse(
-                minute,
-                ore - self.geo_cost_ore,
-                oreR,
-                clay,
-                clayR,
-                obs - self.geo_cost_obs,
-                obsR,
-                geos,
-                geosR + 1,
-            )
-        if (
-            ore >= self.obs_cost_ore
-            and clay >= self.obs_cost_clay
-            and obs < self.obs_max
-        ):
-            self.recurse(
-                minute,
-                ore - self.obs_cost_ore,
-                oreR,
-                clay - self.obs_cost_clay,
-                clayR,
-                obs,
-                obsR + 1,
-                geos,
-                geosR,
-            )
-        if ore >= self.ore_cost_ore and ore < self.ore_max:
-            self.recurse(
-                minute,
-                ore - self.ore_cost_ore,
-                oreR + 1,
-                clay,
-                clayR,
-                obs,
-                obsR,
-                geos,
-                geosR,
-            )
-        if ore >= self.clay_cost_ore and clay < self.clay_max:
-            self.recurse(
-                minute,
-                ore - self.clay_cost_ore,
-                oreR,
-                clay,
-                clayR + 1,
-                obs,
-                obsR,
-                geos,
-                geosR,
-            )
         ore += oreR
         clay += clayR
         obs += obsR
         geos += geosR
+
         minute += 1
+        if obsR and ore >= self.geo_cost_ore and obs >= self.geo_cost_obs:
+            self.recurse(
+                minute,
+                min((ore - self.geo_cost_ore, self.ore_max)),
+                oreR,
+                min((clay, self.obs_cost_clay)),
+                clayR,
+                min((obs - self.geo_cost_obs, self.geo_cost_obs)),
+                obsR,
+                geos - 1,
+                geosR + 1,
+            )
+
+        if (
+            ore >= self.obs_cost_ore
+            and clay >= self.obs_cost_clay
+            and obs < self.geo_cost_obs
+        ):
+            self.recurse(
+                minute,
+                min((ore - self.obs_cost_ore, self.ore_max)),
+                oreR,
+                min((clay - self.obs_cost_clay, self.obs_cost_clay)),
+                clayR,
+                min((obs - 1, self.geo_cost_obs)),
+                obsR + 1,
+                geos,
+                geosR,
+            )
+
+        if ore >= self.clay_cost_ore and clay < self.obs_cost_clay:
+            self.recurse(
+                minute,
+                min((ore - self.clay_cost_ore, self.ore_max)),
+                oreR,
+                min((clay - 1, self.obs_cost_clay)),
+                clayR + 1,
+                min((obs, self.geo_cost_obs)),
+                obsR,
+                geos,
+                geosR,
+            )
+
+        if ore >= self.ore_cost_ore and ore < self.ore_max:
+            self.recurse(
+                minute,
+                min((ore - self.ore_cost_ore - 1, self.ore_max)),
+                oreR + 1,
+                min((clay, self.obs_cost_clay)),
+                clayR,
+                min((obs, self.geo_cost_obs)),
+                obsR,
+                geos,
+                geosR,
+            )
+
         self.recurse(
             minute,
-            ore,
+            min((ore, self.ore_max)),
             oreR,
-            clay,
+            min((clay, self.obs_cost_clay)),
             clayR,
-            obs,
+            min((obs, self.geo_cost_obs)),
             obsR,
             geos,
             geosR,
@@ -168,7 +200,6 @@ class Blueprint:
 
 def get_data(text):
     numbers = re.findall(r"\d+", text)
-    print(numbers)
     numiter = iter(numbers)
     results = []
     for _ in range(len(numbers) // 7):
@@ -176,4 +207,13 @@ def get_data(text):
     return results
 
 
-d = get_data(sample)
+def print_answer(text):
+    then = perf_counter()
+    data = get_data(text)
+    for d in data:
+        d.recurse()
+    print(sum(d.index * d.best_geo for d in data), perf_counter() - then)
+
+
+print_answer(sample)
+print_answer(real)
