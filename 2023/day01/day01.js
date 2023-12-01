e7b95e1789fd9d7969f66560dca7ec5d00caf8f5
Richard <zohassadar@gmail.com>
2023-12-01 09:35:03 -0700

    day 1 part 1

---
+++ 2023/day01/day01.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+data = fs.readFileSync("day01.input").asciiSlice()
+
+to_add = []
+let variable
+data.split("\n").forEach(line => {
+    var numbers = [];
+    [...line.matchAll(/\d/g)].forEach(match => {
+        numbers.push(match[0])
+    }
+    )
+    if (numbers.length){
+        to_add.push(parseInt(`${numbers[0]}${numbers.slice(-1)}`))
+        }
+}
+)
+const initialValue = 0
+const sum = to_add.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+
+console.log(sum)
