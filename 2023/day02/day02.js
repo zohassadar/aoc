48c1eeaaa021cddf6e525acbc59e71e0f60564ec
Richard <zohassadar@gmail.com>
2023-12-02 11:19:44 -0700

    part 2

--- 2023/day02/day02.js
+++ 2023/day02/day02.js
@@ -9,19 +9,30 @@ limits = {
     blue: 14,
 }
 
-new_data = data.trim().split`\n`.map((thing, i) => (
+part1 = [];
+part2 = [];
+
+data.trim().split`\n`.forEach((thing, i) => (
     bad = {
         red: [],
         blue: [],
         green: []
     },
+    maxes = {
+        red: 0,
+        blue: 0,
+        green: 0
+    },
     [...thing.matchAll(/(\d+) (red|green|blue)/g)].forEach(match => {
         value = match[1]
         color = match[2]
         if (parseInt(value) > limits[color]) { bad[color].push(value) }
+        if (parseInt(value) > maxes[color]) { maxes[color] = value }
     }),
-    (bad['red'].length || bad['green'].length || bad['blue'].length) ? 0 : i + 1
+    part1.push((bad['red'].length || bad['green'].length || bad['blue'].length) ? 0 : i + 1),
+    part2.push(maxes['red'] * maxes['green'] * maxes['blue'])
 )
 )
 
-console.log(new_data.reduce((accum, val) => accum + val, 0))
+console.log(part1.reduce((accum, val) => accum + val, 0))
+console.log(part2.reduce((accum, val) => accum + val, 0))
