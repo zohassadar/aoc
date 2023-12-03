7c94db8454d869465cbf7bc577dbfdccbbc23c14
Richard <zohassadar@gmail.com>
2023-12-03 14:51:35 -0700

    more examples

--- 2023/day02/day02.js
+++ 2023/day02/day02.js
@@ -45,3 +45,23 @@ import re; lim=dict(r=12,g=13,b=14); sum( i+1 if 0 not in (int(m[0]) <= lim[m[1]
 import re;p=re.compile(r'(\d+) g|(\d+) r|(\d+) b').findall;m=lambda n,l: max(int(i[n]) if i[n] else 0 for i in l);sum(m(0,p(l))*m(1,p(l))*m(2,p(l)) for l in open('day02.input').read().splitlines())
 
 */
+
+
+/*
+
+Kirjava part 1:
+
+document.body.innerText.trim``.split`\n`.reduce((e,n,r)=>e+=n.replace(/.+: /,[]).split`;`.every(e=>!e.replace(/ ?(\d+) (\w+),? ?/g,(_,n,[r])=>({r:13,g:14,b:15}[r]>n&&[])))?r+1:0,0)
+
+*/
+console.log(data.trim``.split`\n`.reduce((e,n,r)=>e+=n.replace(/.+: /,[]).split`;`.every(e=>!e.replace(/ ?(\d+) (\w+),? ?/g,(_,n,[r])=>({r:13,g:14,b:15}[r]>n&&[])))?r+1:0,0))
+
+
+/*
+
+Kirjava part 2:
+
+document.body.innerText.trim().split`\n`.reduce((e,n)=>(x={r:0,g:0,b:0},n.replace(/(\d+) (\w)/g,(_,n,r)=>{x[r]=Math.max(n,x[r])}),e+x.r*x.g*x.b),0)
+
+
+*/
