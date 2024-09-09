cb3c2e09a50f6587791dbe2b43b754aafa304287
zohassadar <zohassadar@gmail.com>
2024-09-09 21:03:50 +0000

    2023 day 15 part 2

--- 2023/day15/day15.py
+++ 2023/day15/day15.py
@@ -1,4 +1,36 @@
-#part1
-data = open('day15.input').read().strip()
+from functools import reduce
+from collections import OrderedDict
+from pprint import pprint
+import re
 
-print(sum(reduce(lambda a,b:((a+ord(b))*17)%256,t,0) for t in data.split(',')))
+data = open("day15.input").read().strip()
+
+# data="rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+
+elements = data.split(",")
+
+
+def get_hash(element):
+    return reduce(lambda a, b: ((a + ord(b)) * 17) % 256, element, 0)
+
+
+# part1
+print(sum(get_hash(t) for t in elements))
+
+
+# part2
+structure = [OrderedDict() for _ in range(256)]
+
+for element in elements:
+    label, value = re.split(r"[-=]", element)
+    box_id = get_hash(label)
+    if "=" in element:
+        structure[box_id].update({label: int(value)})
+    else:
+        structure[box_id].pop(label, None)
+
+total = 0
+for box_id, box in enumerate(structure, start=1):
+    total += sum(box_id * i * v for i, v in enumerate(box.values(), start=1))
+
+print(total)
