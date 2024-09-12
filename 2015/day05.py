96422a435caadd65089a67d71ca2bbd09f152fea
zohassadar <zohassadar@gmail.com>
2024-09-12 00:14:36 +0000

    2015 day 5 part 1

---
+++ 2015/day05.py
@@ -0,0 +1,28 @@
+import re
+
+data = open('day05.input').read().strip().splitlines()
+
+def vowel_test(string):
+    return len(re.findall(r'[aeiou]', string)) >= 3
+def repeat_test(string):
+    return bool(re.search(r'(.)\1', string))
+def forbidden_test(string):
+    if 'ab' in string:
+        return False
+    if 'cd' in string:
+        return False
+    if 'pq' in string:
+        return False
+    if 'xy' in string:
+        return False
+    return True
+
+def passes_all(string):
+    return vowel_test(string) and repeat_test(string) and forbidden_test(string)
+
+i=0
+for string in data:
+    if passes_all(string):
+        i+=1
+
+print(i)
