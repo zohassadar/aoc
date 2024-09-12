444e740c949255a6099bfc6debe58a41c81cd7b5
zohassadar <zohassadar@gmail.com>
2024-09-12 13:11:08 +0000

    2020 day 1 part 2

--- 2020/day01.py
+++ 2020/day01.py
@@ -8,3 +8,13 @@ for number in data:
     if other in data:
         print(other*number)
         break
+
+
+from itertools import permutations
+
+
+for number,other in permutations(data, 2):
+    otherother = 2020 - (number+other)
+    if otherother in data:
+        print(otherother*other*number)
+        break
