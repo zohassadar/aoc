60c99e6147b1d91a2236814f9c3e8a70c73edd50
zohassadar <zohassadar@gmail.com>
2024-09-12 02:46:59 +0000

    2015 day 6 part 1

---
+++ 2015/day06.py
@@ -0,0 +1,20 @@
+import numpy as np
+import re
+
+data = open('day06.input').read().strip().splitlines()
+
+grid = np.zeros((1000,1000),dtype=np.uint8)
+
+for line in data:
+    a,b,x,y = (int(d) for d in re.findall(r'\d+', line))
+    subgrid = grid[a:x+1,b:y+1]
+    if 'off' in line:
+        subgrid[:] = 0
+    elif 'on' in line:
+        subgrid[:] = 1
+    elif 'toggle' in line:
+        subgrid[:] ^= 1
+
+
+
+print(np.count_nonzero(grid))
