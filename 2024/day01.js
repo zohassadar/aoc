b994ff8bac0265eb9a005849537e895c1c2374a3
zohassadar <zohassadar@gmail.com>
2024-12-01 21:52:39 +0000

    day 1 part 2

--- 2024/day01.js
+++ 2024/day01.js
@@ -1,1 +1,3 @@
 a=[];b=[];document.body.innerText.trim().split('\n').map(c=>c.split(/\s+/)).map(d=>(a.push(+d[0]),b.push(+d[1])));a.sort();b.sort();a.map((e,i)=>[e,b[i]]).reduce((a,v)=>a+Math.abs(v[0]-v[1]),0)
+
+a=[];b=[];document.body.innerText.trim().split('\n').map(c=>c.split(/\s+/)).map(d=>(a.push(+d[0]),b.push(+d[1])));a.reduce((a,z)=>a+b.filter(c=>c==z).length*z,0)
