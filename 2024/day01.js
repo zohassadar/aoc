2b97439848aafae565ab6b40734ee96be22a5602
zohassadar <zohassadar@gmail.com>
2024-12-01 21:42:47 +0000

    2024 day 1 part 1

---
+++ 2024/day01.js
@@ -0,0 +1,1 @@
+a=[];b=[];document.body.innerText.trim().split('\n').map(c=>c.split(/\s+/)).map(d=>(a.push(+d[0]),b.push(+d[1])));a.sort();b.sort();a.map((e,i)=>[e,b[i]]).reduce((a,v)=>a+Math.abs(v[0]-v[1]),0)
