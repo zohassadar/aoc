28201fb71584aa565512fb84c9cce787c760bdb7
zohassadar <zohassadar@gmail.com>
2024-12-02 22:12:26 +0000

    Update day08.py

--- 2015/day08.py
+++ 2015/day08.py
@@ -8,3 +8,31 @@ for d in data:
     v+=len(eval(d))
 
 print(c-v)
+
+data = open('day8input', 'rb').read()
+
+v2 = 0
+i = 0
+while True:
+    if i == len(data):
+        break
+    if data[i] == 0x0a:
+        i+=1
+        continue
+    if tuple(data[i:i+2]) == (0x5c,0x22):
+        i+=2
+        v2+=4
+    elif tuple(data[i:i+2]) == (0x5c, 0x78):
+        i+=4
+        v2+=5
+    elif tuple(data[i:i+2]) == (0x5c, 0x5c):
+        i+=2
+        v2+=4
+    elif data[i] == 0x22:
+        i+=1
+        v2+=3
+    else:
+        i+=1
+        v2+=1
+
+print(v2-c)
