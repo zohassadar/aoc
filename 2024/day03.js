8ca2f792d6d9d17a509186e2b9d224ba4623eaf1
zohassadar <zohassadar@gmail.com>
2024-12-03 16:49:20 +0000

    Update day03.js

--- 2024/day03.js
+++ 2024/day03.js
@@ -1,2 +1,5 @@
 //part 1
 Array.from(document.body.innerText.matchAll(/mul\((\d+),(\d+)\)/g), m=>m[1]*m[2]).reduce((a,v)=>a+v,0)
+
+// part 2
+f=1;Array.from(document.body.innerText.matchAll(/mul\((\d+),(\d+)\)|do(?:n't)?\(\)/g),m=>m[0][0]=="d"?m[0][2]=="n"?f=0:(f=1,0):f?m[1]*m[2]:0).reduce((a,v)=>a+v,0)
