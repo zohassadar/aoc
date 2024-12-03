25cae52a2ceb75705d6d22e919589151f2e5c738
zohassadar <zohassadar@gmail.com>
2024-12-03 20:56:59 +0000

    Update day02.js

--- 2020/day02.js
+++ 2020/day02.js
@@ -1,2 +1,5 @@
 // part 1
 document.body.innerText.trim().split(/\n/).map(a=>a.match(/(\d+)-(\d+) (\w): (\w+)/)).reduce((a,v)=>a+(l=(v[4].split(v[3]).length)-1,r=v[1]<=l&&l<=v[2]),0)
+
+// part 2
+document.body.innerText.trim().split(/\n/).map(a=>a.match(/(\d+)-(\d+) (\w): (\w+)/)).reduce((a,v)=>a+((v[4][+v[1]-1]==v[3])+(v[4][+v[2]-1]==v[3])==1),0)
