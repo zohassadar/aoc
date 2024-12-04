ac2527c48c4f994e9dc210d2fb49dc323ca2d5bc
zohassadar <zohassadar@gmail.com>
2024-12-04 00:41:14 +0000

    Update day02.js

--- 2017/day02.js
+++ 2017/day02.js
@@ -1,2 +1,18 @@
 //part 1
 document.body.innerText.trim().split(/\n/).reduce((a,v)=>(n=v.split(/\s+/),a+Math.max(...n)-Math.min(...n)),0) 
+
+// part 2
+t = 0;
+document.body.innerText
+    .trim()
+    .split(/\n/)
+    .map(
+        (v) => (
+            (n = v.split(/\s+/).sort((a, b) => b - a)),
+            n.forEach((o, i) =>
+                n.slice(i + 1).forEach((p) => !(o % p) && (t += o / p)),
+            )
+        ),
+    );
+t;
+
